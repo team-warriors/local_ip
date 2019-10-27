7ab44608a4b18f6cbd33dbd05f5024d66a8c27e8
Adiatma Kamarudin <adiatma.kamaarudin@ralali.com>
2019-10-27 09:36:36 +0700

    docs: update

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,11 @@ pub mod get;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use get;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_get_to_string() {
+        assert_eq!(get::to_string(), Some(String::from("{\"local\":\"127.0.0.1\",\"network\":\"192.168.43.102\"}")));
     }
 }
